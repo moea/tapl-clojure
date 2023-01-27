9be520003757d4acea04cb581c98ec26e3e7673a
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2023-01-27 22:28:27 +0000

    Small changes

--- src/tapl_clojure/untyped_lc.clj
+++ src/tapl_clojure/untyped_lc.clj
@@ -5,14 +5,15 @@
 (defn term-walk [on-var c t]
   (let [walk (fn walk [c t]
                (match t
-                 [:term/var x  n]   (on-var c x n)
+                 [:term/var x  t]   (on-var c x t)
                  [:term/abs x  t']  [:term/abs x (walk c t')]
                  [:term/app t' t''] [:term/app (walk c t') (walk c t'')]))]
     (walk c t)))
 
 (defn term-shift-above [d c t]
   (term-walk
-   (fn [c x n] [:term/var (cond-> x (<= c x) (+ d)) (+ n d)])
+   (fn [c x n]
+     [:term/var (cond-> x (<= c x) (+ d)) (+ n d)])
    c t))
 
 (defn term-shift [d t]
@@ -20,7 +21,10 @@
 
 (defn term-sub [j s t]
   (term-walk
-   (fn [c x n] (if (= x (+ j c)) (term-shift c s) [:term/var x n]))
+   (fn [c x n]
+     (if (= x (+ j c))
+       (term-shift c s)
+       [:term/var x n]))
    0 t))
 
 (defn term-sub-top [s t]
@@ -31,10 +35,10 @@
 
 (defn eval1 [ctx t]
   (match t
-    ([:term/app [:term/abs _ t12] v] :guard #(is-val? (last %)))   (term-sub-top v t12)
-    ([:term/app _ _]                 :guard #(is-val? (second %))) (update t 2 (partial eval1 ctx))
-    [:term/app  _ _]                                                (update t 1 (partial eval1 ctx))
-    :else                                                           (throw (ex-info "No rule applies" {::term t})))  )
+    ([:term/app [:term/abs _ t12] v] :guard (comp is-val? last))   (term-sub-top v t12)
+    ([:term/app _ _]                 :guard (comp is-val? second)) (update t 2 (partial eval1 ctx))
+    [:term/app  _ _]                                               (update t 1 (partial eval1 ctx))
+    :else                                                          (throw (ex-info "No rule applies" {::term t})))  )
 
 (defn eval [ctx t]
   (let [ctx (into (sorted-map) ctx)]
