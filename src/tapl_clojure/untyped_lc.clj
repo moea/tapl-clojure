f54d2dc8ce83d874815059ab99df567b2304db73
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2023-02-20 20:33:20 +0000

    Private fns

--- src/tapl_clojure/untyped_lc.clj
+++ src/tapl_clojure/untyped_lc.clj
@@ -56,7 +56,7 @@
                  :else                   node))]
     (walk/prewalk str (trec expr []))))
 
-(defn rewrite-vars [f c t]
+(defn- rewrite-vars [f c t]
   (let [walk (fn walk [c t]
                (match t
                  [:term/var x  t']  (f c x t')
@@ -64,16 +64,16 @@
                  [:term/app t' t''] [:term/app (walk c t') (walk c t'')]))]
     (walk c t)))
 
-(defn term-shift-above [d c t]
+(defn- term-shift-above [d c t]
   (rewrite-vars
    (fn [c x n]
      [:term/var (cond-> x (<= c x) (+ d)) (+ n d)])
    c t))
 
-(defn term-shift [d t]
+(defn- term-shift [d t]
   (term-shift-above d 0 t))
 
-(defn term-sub [j s t]
+(defn- term-sub [j s t]
   (rewrite-vars
    (fn [c x n]
      (if (= x (+ j c))
@@ -81,16 +81,16 @@
        [:term/var x n]))
    0 t))
 
-(defn term-sub-top [s t]
+(defn- term-sub-top [s t]
   (term-shift (- 1) (term-sub 0 (term-shift 1 s) t)))
 
-(defn is-val? [x]
+(defn- is-abs? [x]
   (and (vector? x) (identical? (first x) :term/abs)))
 
-(defn eval1 [ctx t]
+(defn- eval1 [ctx t]
   (match t
-    ([:term/app [:term/abs _ t12] v] :guard (comp is-val? last))   (term-sub-top v t12)
-    ([:term/app _ _]                 :guard (comp is-val? second)) (update t 2 (partial eval1 ctx   ))
+    ([:term/app [:term/abs _ t12] v] :guard (comp is-abs? last))   (term-sub-top v t12)
+    ([:term/app _ _]                 :guard (comp is-abs? second)) (update t 2 (partial eval1 ctx   ))
     [:term/app  _ _]                                               (update t 1 (partial eval1 ctx))
     :else                                                          (throw (ex-info "No rule applies" {::term t}))))
 
@@ -99,7 +99,7 @@
 
 (defn eval
   ([ctx t]
-   (let [t (cond-> t (string? t) (-> parse de-brujin))
+   (let [t   (cond-> t (string? t) (-> parse de-brujin))
          res (try
                (eval' ctx t)
                (catch clojure.lang.ExceptionInfo e
